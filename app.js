3cddefe489a42365594d684cd3f5c031deb33836
Siddhartha <siddharthamyakala042@gmail.com>
2023-01-26 17:17:30 +0530

    1st commit

---
+++ app.js
@@ -0,0 +1,10 @@
+(function(){
+    'use strict';
+    angular.module('Calci',[])
+    .controller('calciController',function($scope){
+        $scope.name = "Siddu"
+        console.log("Working");
+    })
+
+
+})();
